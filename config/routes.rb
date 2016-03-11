6a712265a44b4972ceef1ad4afc299fab22ee350
Ethan-Rivas <ethan-rivas@hotmail.com>
2016-03-10 18:33:15 -0600

    api players route

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,11 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   root to: 'frontend#show'
+
+  namespace :api do
+    namespace :v1 do
+      resources :players
+    end
+  end
+
 end

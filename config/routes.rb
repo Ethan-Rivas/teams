5a3c4c710db0f3d6f8afc23432299ac1ebeea2d4
Ethan-Rivas <ethan-rivas@hotmail.com>
2016-03-15 17:56:43 -0600

    devise for users in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
+  devise_for :users
   root to: 'frontend#show'
-
   namespace :api do
     namespace :v1 do
       resources :players
     end
   end
-
 end
